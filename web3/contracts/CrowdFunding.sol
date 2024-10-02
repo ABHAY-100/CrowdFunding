7532ed54aae77cd097855d61bdb3d89ee6435b7b
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2024-10-02 16:02:14 +0530

    completed smart contract

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -18,10 +18,20 @@ contract CrowdFunding {
 
     uint256 public numberOfCampaigns = 0;
 
-    function createCampaign(address owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) { 
+    function createCampaign(
+        address owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256) {
         Campaign storage campaign = campaigns[numberOfCampaigns];
 
-        require(campaign.deadline > block.timestamp, "The deadline should be a date in the future");
+        require(
+            campaign.deadline > block.timestamp,
+            "The deadline should be a date in the future"
+        );
 
         campaign.owner = owner;
         campaign.title = _title;
@@ -46,12 +56,26 @@ contract CrowdFunding {
 
         (bool sent, ) = payable(campaign.owner).call{value: amount}("");
 
-        if(sent) {
+        if (sent) {
             campaign.amountCollected += amount;
         }
     }
 
-    // function getDonators() {}
+    function getDonators(
+        uint256 _id
+    ) public view returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
 
-    // function getCampaigns() {}
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for (uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage campaign = campaigns[i];
+
+            allCampaigns[i] = campaign;
+        }
+
+        return allCampaigns;
+    }
 }
